3e3edad1e66039fb30bc44f330cf617442c2844c
anil-ninja <kumar.anil8892@gmail.com>
2015-02-21 16:47:24 +0530

    image added

---
+++ ajax/subscribe.php
@@ -0,0 +1,17 @@
+<?php
+session_start() ;
+	$config['host'] = "localhost" ;
+	$config['user'] = "root" ;
+	$config['password'] = "redhat111111" ;
+	$config['database'] = "todfodcode" ;
+	$db_handle = mysqli_connect($config['host'], $config['user'], $config['password'], $config['database']);
+if($_POST['id']){
+	$project = $_POST['id'];
+	$time = date("Y-m-d H:i:s") ;
+	$myquery = mysqli_query($db_handle, "INSERT INTO targets (email) VALUES ('$project') ;") ;
+	if(mysqli_error($db_handle)) { echo "Failed to Post!"; }
+	else { echo "Subscribed succesfully!"; }
+}
+else {echo "Invalid";}
+mysqli_close($db_handle);	
+?>
